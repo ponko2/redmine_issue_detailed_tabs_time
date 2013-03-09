0073e9b5c5fb81d9fbf9894e94d9caae5b5d43c2
markedagain <markkalender@hotmail.com>
2013-03-09 10:39:06 -0500

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'redmine'
 require 'issue_tabs_listener.rb'
 
-Redmine::Plugin.register :redmine_issue_history_tabs do
+Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
