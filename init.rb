40b68f81edb8ab25aec1ff3e22b8084ca40162a9
root <root@LampWorkDev.(none)>
2013-03-10 18:06:38 -0400

    fixed name issues

--- init.rb
+++ init.rb
@@ -5,5 +5,5 @@ Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
-  version '0.0.6'
+  version '0.0.7'
 end
