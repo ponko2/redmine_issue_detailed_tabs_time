a5d9901f9b508e6b6733903f8489f2e33ff4df94
Daniel Felix <d.felix@proinsider.net>
2013-08-25 17:26:38 +0200

    Restructured issue-history-tabs.js and simplified it

--- assets/javascripts/issue-history-tabs.js
+++ assets/javascripts/issue-history-tabs.js
@@ -5,33 +5,25 @@ Forked & Redone: Mark Kalender (Markedagain)
 Date: 08/03/2013
 */
 function init_tabs() {
-	bindTab('#tab-tabtime_questions', '.journal.question,.journal.question-closed');
-	bindTab('#tab-tabtime_time', '.journal.has-time');
-	bindTab('#tab-history_private', '.journal.private-notes');
-	bindTab('#tab-history_comments', '.journal.has-notes');
-	bindTab('#tab-history_activity', '.journal.has-details');
-	bindTab('#tab-history_all', '.journal');
-	selectecTab = $('.tab-history.selected')[0].id
-	if (selectecTab != 'tab-history_all'){$('#'+selectecTab).click();};
-	
+	bindTab('tabtime_questions', '.journal.question,.journal.question-closed');
+	bindTab('tabtime_time', '.journal.has-time');
+	bindTab('history_private', '.journal.private-notes');
+	bindTab('history_comments', '.journal.has-notes');
+	bindTab('history_activity', '.journal.has-details');
+	bindTab('history_all', '.journal');
+	$('.tab-history.selected').not('#tab-history_all')[0] && $('.tab-history.selected').not('#tab-history_all')[0].click();
 }
-function bindTab(tabName , journal){
-	$(tabName).click(function(e){
-		$('.tab-history').removeClass('selected')
-		$(tabName).addClass('selected')
+
+function bindTab(tab, journal){
+	$('#tab-'+tab).click(function(){
+		$('.tab-history').removeClass('selected');
+		$('#tab-'+tab).addClass('selected');
 		if("replaceState" in window.history){
-			url = '?tab='+tabName.replace('#tab-','');
-			window.history.replaceState(null,document.title,url);
-		} 
-		show_history($(journal));
-	 })
-}
-function doClick(tabName, journal){
-	
-}
-function show_history(tag) {
-  $('.journal').hide();
-  tag.show();
+			window.history.replaceState(null,document.title,'?tab='+tab);
+		}
+		$('.journal').hide();
+		$(journal).show();
+	});
 }
 
 $(document).ready(function(){
