0bfd2684a3403de5dccaf668adbbcb9038d8afa4
Daniel Felix <d.felix@proinsider.net>
2013-08-28 16:45:01 +0200

    Corrected version number

--- init.rb
+++ init.rb
@@ -12,5 +12,5 @@ Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
-  version '0.0.7'
+  version '0.1.0'
 end
