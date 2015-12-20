6ad699dfa9cf312490bd0fb83ba2f5e8abf2df3a
Will Richardson <willr@catalyst.net.nz>
2015-12-21 10:39:57 +1300

    Selected tab saved in cookie so it persists across pages

--- assets/javascripts/issue-history-tabs.js
+++ assets/javascripts/issue-history-tabs.js
@@ -4,6 +4,26 @@ Date: 23/05/2012
 Forked & Redone: Mark Kalender (Markedagain)
 Date: 08/03/2013
 */
+// Simple cookie handling from http://stackoverflow.com/a/24103596/692410
+function createCookie(name,value) {
+	document.cookie = name + "=" + value + "; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1, c.length);
+		}
+		if (c.indexOf(nameEQ) == 0){
+			return c.substring(nameEQ.length, c.length);
+		}
+	}
+	return null;
+}
+
 function initTabs() {
 	bindTab('tabtime_questions', '.journal.question,.journal.question-closed');
 	bindTab('tabtime_time', '.journal.has-time');
@@ -11,8 +31,8 @@ function initTabs() {
 	bindTab('history_comments', '.journal.has-notes');
 	bindTab('history_activity', '.journal.has-details');
 	bindTab('history_all', '.journal');
-  bindTab('history_none', '');
-	$('.tab-history.selected').not('#tab-history_all')[0] && $('.tab-history.selected').not('#tab-history_all')[0].click();
+	bindTab('history_none', '');
+	selectDefaultTab();
 }
 
 function bindTab(tab, journal){
@@ -24,9 +44,22 @@ function bindTab(tab, journal){
 		}
 		$('.journal').hide();
 		$(journal).show();
+		createCookie('selected_issue_tab', tab);
 	});
 }
 
+function selectDefaultTab() {
+	var tab = readCookie('selected_issue_tab');
+	if(tab != null) {
+		var elem = $('#tab-' + tab);
+		if(elem) {
+			elem.click();
+			return;
+		}
+	}
+	$('.tab-history.selected').not('#tab-history_all')[0] && $('.tab-history.selected').not('#tab-history_all')[0].click();
+}
+
 $(document).ready(function(){
-  initTabs();
+	initTabs();
 });
