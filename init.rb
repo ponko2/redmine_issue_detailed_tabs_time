003e63a26205e9a8e11fe913e08e62e943c62351
Takahito Nakano <kano@ponko2.jp>
2023-04-12 11:57:48 +0900

    Update for Redmine 5.x

--- init.rb
+++ init.rb
@@ -1,6 +1,10 @@
 require 'redmine'
-require 'issue_tabs_listener'
-require_dependency 'issue_detailed_tabs_time_issues_helper_patch'
+
+unless Rails.version > '6.0' && Rails.autoloaders.zeitwerk_enabled? then
+  require_relative './lib/issue_tabs_view_listener'
+end
+
+require_dependency File.expand_path('../lib/redmine_issue_detailed_tabs_time_issues_helper_patch', __FILE__)
 
 Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
 
@@ -13,5 +17,5 @@ Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
-  version '0.1.0'
+  version '0.1.1'
 end
